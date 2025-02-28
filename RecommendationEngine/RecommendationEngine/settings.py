a36637e99d23d63adc0cfa768f2f100be79e3340
Svetlana Greipel <svetlana.greipel@drake.edu>
2025-02-28 10:22:35 -0600

    completed login/logout/reset password

--- RecommendationEngine/RecommendationEngine/settings.py
+++ RecommendationEngine/RecommendationEngine/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -119,5 +120,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
