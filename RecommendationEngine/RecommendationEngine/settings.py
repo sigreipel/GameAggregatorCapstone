e7e5fc84ad5c82775137e10afef32b3b48d6d62f
Faith Bailey <faith.bailey@drake.edu>
2025-03-06 16:41:35 -0600

    updated nav bar, base, home, login, and discover

--- RecommendationEngine/RecommendationEngine/settings.py
+++ RecommendationEngine/RecommendationEngine/settings.py
@@ -129,3 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 DISCOVER_REDIRECT_URL = "discover"
+NAVLOGIN_REDIRECT_URL = "logIn"
