98c8f2cab173c0842e952fe906ea8fc59ea7d2e5
Faith Bailey <faith.bailey@drake.edu>
2025-04-22 19:20:35 -0500

    changed settings a bit

--- RecommendationEngine/RecommendationEngine/settings.py
+++ RecommendationEngine/RecommendationEngine/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f%q(_s#(va2(%jyw*w-e13iqjbxcq^-0^=u1wuf&6#am^p5puk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.209.65.117', 'localhost', '127.0.0.1']
 
 
 # Application definition
