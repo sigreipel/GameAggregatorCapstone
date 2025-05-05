2f0c8ea32e429a6072c3f4b05a124d25810dc31c
Faith Bailey <faith.bailey@drake.edu>
2025-05-05 10:59:45 -0500

    changed discover and views

--- RecommendationEngine/accounts/views.py
+++ RecommendationEngine/accounts/views.py
@@ -4,9 +4,15 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 from .models import CustomUser
+from django.shortcuts import render
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+    
+
+def discover(request):
+    return render(request, 'discover.html', {'range': range(6)})
+
 
