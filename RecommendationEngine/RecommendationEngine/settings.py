f77abc58c1ccd5254f94987f31c08380d843c588
Faith Bailey <faith.bailey@drake.edu>
2025-05-04 20:17:59 -0500

    fixed DEBUG

--- RecommendationEngine/RecommendationEngine/settings.py
+++ RecommendationEngine/RecommendationEngine/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f%q(_s#(va2(%jyw*w-e13iqjbxcq^-0^=u1wuf&6#am^p5puk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
